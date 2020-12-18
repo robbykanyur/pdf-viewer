4312cbcea646804093cc67c7f16f77efe382987e
Robby Kanyur <robby@kanyur.com>
2020-12-17 20:30:41 -0700

    Load images sequentially and add loading indicator

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,31 @@ var number_of_images = 10;
 var button_back = document.getElementById('button-back');
 var button_next = document.getElementById('button-next');
 
+var images = [
+    'img/01-front.jpg',
+    'img/02-about.jpg',
+    'img/03-fairway.jpg',
+    'img/04-products.jpg',
+    'img/05-fairway-now.jpg',
+    'img/06-fairway-next.jpg',
+    'img/07-creditool.jpg',
+    'img/08-listreports.jpg',
+    'img/09-covid19.jpg',
+    'img/10-cover-back.jpg'
+];
+
+function loadImage(counter) {
+    if (counter >= (images.length)) {
+        return;
+    } else {
+        var i = document.getElementById('image-' + (counter + 1)).firstChild;
+        i.onload = i.onerror = function() { loadImage(counter + 1)}
+        i.src = images[counter];
+    }
+};
+
+loadImage(0);
+
 function updateStatus() {
     status_div = document.getElementById('current-page');
     status_div.innerHTML = current_image;
